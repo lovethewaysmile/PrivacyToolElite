4c281a42b89d743e1870c5252d1f7949d69d86fa
lovethewaysmile <lovethewaysmile@proton.me>
2025-10-27 19:42:04 +0100

    Add PrivacyVoteHubAddresses.ts

---
+++ auroravote-frontend/abi/PrivacyVoteHubAddresses.ts
@@ -0,0 +1,1 @@
+export const PrivacyVoteHubAddresses = { "31337": { address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512" }, "11155111": { address: "0x7Da4cBB7aEf12f4ebD1EbcD4B6eae00E2934bfF3" } } as const;
